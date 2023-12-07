a418940c6143387f4a293a29ccbf2010a53d8d7f
Binnur KURT <deepcloudlabs@gmail.com>
2023-12-07 15:07:46 +0300

    Update FeignLotteryService.java

--- lottery-consumer-microservice/src/main/java/com/example/lottery/client/FeignLotteryService.java
+++ lottery-consumer-microservice/src/main/java/com/example/lottery/client/FeignLotteryService.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "lottery")
 public interface FeignLotteryService {
-	@GetMapping(value = "/lottery/api/v1/numbers", params = "column")
+	@GetMapping(value = "/api/v1/numbers", params = "column")
 	public List<List<Integer>> draw(@RequestParam int column);
 }
