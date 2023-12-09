7d8bc67a64b6ab2a4f5fe2b31b58fbc24afd0b79
Binnur KURT <deepcloudlabs@gmail.com>
2023-12-09 10:39:04 +0300

    Update BusinessService.java

--- resiliency-patterns/src/main/java/com/example/service/BusinessService.java
+++ resiliency-patterns/src/main/java/com/example/service/BusinessService.java
@@ -1,14 +1,19 @@
 package com.example.service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @Service
 public class BusinessService {
-	@Retry(name = "fun",fallbackMethod = "gun")
+
+	@Retry(name = "fun", fallbackMethod = "gun")
 	public int fun() {
 		if (ThreadLocalRandom.current().nextBoolean()) {
 			System.err.println("fun() is about to fail...");
@@ -16,8 +21,32 @@ public class BusinessService {
 		}
 		return 42;
 	}
-	
+
+	@RateLimiter(name = "fun", fallbackMethod = "run")
+	@Retry(name = "fun", fallbackMethod = "gun")
+	public int sun() {
+		return 42;
+	}
+
 	public int gun(Throwable e) {
 		return 108;
+	}
+
+	public int run(Throwable e) {
+		return 108;
+	}
+	
+	@TimeLimiter(name="methoda",fallbackMethod = "methodAFallback")
+	public CompletableFuture<Integer> methodA(){
+		return CompletableFuture.supplyAsync(()->{
+			try {TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 6));}catch (Exception e) {}
+			return 42;
+		});
+	}
+
+	public CompletableFuture<Integer> methodAFallback(Throwable e){
+		return CompletableFuture.supplyAsync(()->{
+			return 108;
+		});
 	}
 }
